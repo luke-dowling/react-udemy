34a7bb84024266425cac5b4762588593a564a158
Luke Dowling <lpdowling26@googlemail.com>
2022-01-16 20:24:57 +0100

    added form and styling

---
+++ module-27-typescript-react/react-ts/src/components/NewTodo.tsx
@@ -0,0 +1,29 @@
+import { useRef } from "react";
+import classes from "./NewTodo.module.css";
+
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+
+  const sumbitHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const enteredText = todoTextInputRef.current!.value;
+
+    if (enteredText.trim().length === 0) {
+      // throw error
+      return;
+    }
+
+    props.onAddTodo(enteredText);
+  };
+
+  return (
+    <form action="" onSubmit={sumbitHandler} className={classes.form}>
+      <label htmlFor="text">Todo text</label>
+      <input type="text" id="text" ref={todoTextInputRef} />
+      <button>Add Todo</button>
+    </form>
+  );
+};
+
+export default NewTodo;
--- module-27-typescript-react/react-ts/src/components/Todos.tsx
+++ module-27-typescript-react/react-ts/src/components/Todos.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import classes from "./Todos.module.css";
 
 import Todo from "../models/todo";
 import ListItem from "./ListItem";
@@ -6,7 +6,7 @@ import ListItem from "./ListItem";
 // React.FC is already generic so the <> is what we us to state the type inside
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
-    <ul>
+    <ul className={classes.todos}>
       {props.items.map((item) => (
         <ListItem key={item.id} text={item.text} />
       ))}
